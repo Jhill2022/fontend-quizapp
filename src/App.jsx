31adfa50d03e16ebaeec8a799e80bf1eb59d1756
Jhill2022 <jhill42015@gmail.com>
2025-01-01 19:45:53 -0600

    styling quiz and app component

--- src/App.jsx
+++ src/App.jsx
@@ -68,45 +68,47 @@ function App() {
   return (
     <div>
       {!title ? (
-        <div
-          className={`min-h-screen ${
-            isDarkMode
-              ? 'bg-[#313E51] bg-dark-mode text-white'
-              : 'bg-[#F4F6FA] text-[black] bg-light-mode'
-          } px-7 pt-7 md:px-16 lg:px-36 lg:pt-20`}
-        >
-          <div className="flex justify-end mb-14 lg:mb-20">
-            <LightToDarkSwitch
-              handleDarkMode={handleDarkMode}
-              isDarkMode={isDarkMode}
-            />
-          </div>
-          <div className="flex flex-col lg:flex-row w-full lg:gap-12">
-            <div>
-              <div className="leading-tight">
-                <h1 style={isDarkMode ? { color: 'white' } : { color: '#313E51' }} className="text-[40px] md:text-[64px] font-light">Welcome to the</h1>
-                <span style={isDarkMode ? { color: 'white' } : { color: '#313E51' }} className="font-medium text-[40px] text-[#313E51] md:text-[64px]">Frontend Quiz!</span>
+        <div className={`min-h-screen ${
+          isDarkMode
+            ? 'bg-[#313E51] bg-dark-mode text-white'
+            : 'bg-[#F4F6FA] text-[black] bg-light-mode'
+        }`}>
+          <div
+            className={` px-7 pt-7 md:px-16 lg:px-36 lg:pt-20 ` }
+          >
+            <div className="flex justify-end mb-14 lg:mb-20">
+              <LightToDarkSwitch
+                handleDarkMode={handleDarkMode}
+                isDarkMode={isDarkMode}
+              />
+            </div>
+            <div className="flex flex-col lg:flex-row w-full lg:gap-12">
+              <div>
+                <div className="leading-tight">
+                  <h1 style={isDarkMode ? { color: 'white' } : { color: '#313E51' }} className="text-[40px] md:text-[64px] font-light">Welcome to the</h1>
+                  <span style={isDarkMode ? { color: 'white' } : { color: '#313E51' }} className="font-medium text-[40px] text-[#313E51] md:text-[64px]">Frontend Quiz!</span>
+                </div>
+                <p className="text-[#626C7F] mt-4 mb-10">
+                  <em className={`${isDarkMode ? 'text-[#ABC1E1]' : "text-[#626C7F]"} font-light`}>
+                    Pick a subject to get started
+                  </em>
+                </p>
               </div>
-              <p className="text-[#626C7F] mt-4 mb-10">
-                <em className={`${isDarkMode ? 'text-[#ABC1E1]' : "text-[#626C7F]"} font-light`}>
-                  Pick a subject to get started
-                </em>
-              </p>
+              <div className="lg:flex-grow">
+                <ul className="flex flex-col gap-3 lg:gap-4">
+                  {data.quizzes.map((quizItem) => (
+                    <QuizTitle
+                      title={quizItem}
+                      handleTitle={handleTitle}
+                      key={quizItem.title}
+                      isDarkMode={isDarkMode}
+                    />
+                  ))}
+                </ul>
+              </div>
+                      </div>
             </div>
-            <div className="lg:flex-grow">
-              <ul className="flex flex-col gap-3 lg:gap-4">
-                {data.quizzes.map((quizItem) => (
-                  <QuizTitle
-                    title={quizItem}
-                    handleTitle={handleTitle}
-                    key={quizItem.title}
-                    isDarkMode={isDarkMode}
-                  />
-                ))}
-              </ul>
-            </div>
-                    </div>
-          </div>
+        </div>
       ) : isQuizCompleted ? (
         <div className={`px-6 py-8 md:px-16 bg-[#F4F6FA] min-h-screen ${isDarkMode
           ? 'bg-[#313E51] bg-dark-mode text-white'
@@ -117,28 +119,29 @@ function App() {
               <LightToDarkSwitch isDarkMode={isDarkMode} handleDarkMode={handleDarkMode}/>
             </div>
           </div>
-          <div className="mb-10">
-            <p className="text-4xl md:text-[64px] font-light">Quiz completed</p>
-            <p className="text-4xl md:text-[64px] font-medium">You scored...</p>
-          </div>
-
-          <div className="flex flex-col items-center rounded-2xl gap-3 mb-3 py-8 bg-white">
-            <div className="flex items-center gap-3">
-              <div
-                style={{ backgroundColor: titleColors[title] }}
-                className=" p-1 rounded-md flex items-center justify-center"
-              >
-                <img src={icon} alt="" />
-              </div>
-              <h2 className="text-lg font-medium">{title}</h2>
+          <div>
+            
+            <div className="mb-10">
+              <p className="text-4xl md:text-[64px] font-light">Quiz completed</p>
+              <p className="text-4xl md:text-[64px] font-medium">You scored...</p>
             </div>
-
-            <h2 className="text-8xl text-[#313E51]">{score}</h2>
-            <p className="text-[#626C7F] text-lg">out of {quiz.length}</p>
+            <div className="flex flex-col items-center rounded-2xl gap-3 mb-3 py-8 bg-white">
+              <div className="flex items-center gap-3">
+                <div
+                  style={{ backgroundColor: titleColors[title] }}
+                  className=" p-1 rounded-md flex items-center justify-center"
+                >
+                  <img src={icon} alt="" />
+                </div>
+                <h2 className="text-lg font-medium">{title}</h2>
+              </div>
+              <h2 className="text-8xl text-[#313E51]">{score}</h2>
+              <p className="text-[#626C7F] text-lg">out of {quiz.length}</p>
+            </div>
+            <button onClick={handleReset} className="py-3 md:mt-8 md:py-8 w-full bg-purple-500 text-white text-lg rounded-2xl">
+              Play Again
+            </button>
           </div>
-          <button onClick={handleReset} className="py-3 md:mt-8 md:py-8 w-full bg-purple-500 text-white text-lg rounded-2xl">
-            Play Again
-          </button>
         </div>
       ) : (
         <Quiz
--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -5,7 +5,22 @@ import incorrect from "./images/icon-incorrect.svg";
 import { TitleWColors } from "./TitleWColors";
 
 export function Quiz({
-  title, icon, quiz, currentQuestionIndex, setAnswered, answered, handleNextQuestion, selectedOption, setSelectedOption, setIsSubmitted, isSubmitted, pendingOption, setPendingOption, setScore, handleDarkMode, isDarkMode,
+  title,
+  icon,
+  quiz,
+  currentQuestionIndex,
+  setAnswered,
+  answered,
+  handleNextQuestion,
+  selectedOption,
+  setSelectedOption,
+  setIsSubmitted,
+  isSubmitted,
+  pendingOption,
+  setPendingOption,
+  setScore,
+  handleDarkMode,
+  isDarkMode,
 }) {
   const currentQuestion = quiz[currentQuestionIndex];
 
@@ -27,100 +42,132 @@ export function Quiz({
     setIsSubmitted(true); // Mark the question as answered
   }
   return (
-    <div className={`px-7 py-5 lg:pt-20 md:px-16 ${isDarkMode ? 'bg-[#313E51] bg-[url("./images/pattern-background-mobile-dark.svg")] text-white' : 'bg-[#F4F6FA] bg-[url("./images/pattern-background-mobile-light.svg")]'}  min-h-screen `}>
+    <div
+      className={`px-7 py-5 lg:pt-20 md:px-16 lg:px-36 ${
+        isDarkMode
+          ? 'bg-[#313E51] bg-dark-mode text-white'
+          : 'bg-[#F4F6FA] bg-light-mode'
+      }  min-h-screen`}
+    >
       <div className="flex justify-between items-center mb-12">
-        <TitleWColors title={title} icon={icon}/>
+        <TitleWColors title={title} icon={icon} />
         <div className="">
           <LightToDarkSwitch
             handleDarkMode={handleDarkMode}
-            isDarkMode={isDarkMode} />
+            isDarkMode={isDarkMode}
+          />
         </div>
       </div>
-      <p className="mb-3 text-[#626C7F]">
-        <em className={`${isDarkMode ? 'text-[#ABC1E1]' : 'text-[#626C7F]'} font-light`}>
-          Question {currentQuestionIndex + 1} of {quiz.length}
-        </em>
-      </p>
-      <h3 className="text-2xl font-semibold mb-6">
-        {currentQuestion.question}
-      </h3>
-      <div className={`w-full ${isDarkMode
-        ? 'bg-[#3B4D66]' : 'bg-white '} rounded-full h-3 mb-10 flex items-center`}>
-        <div
-          className="bg-purple-500 h-2 rounded-full "
-          style={{ width: `${progressPercentage}%` }}
-        ></div>
-      </div>
-      <ul className="mb-4">
-        {currentQuestion.options.map((option, index) => (
-          <div key={index}>
-            <li
-              style={{
-                border: isSubmitted && pendingOption === option
-                  ? `2px solid ${option === currentQuestion.answer ? "green" : "red"}`
-                  : pendingOption === option
-                    ? "2px solid #A729F5"
-                    : "transparent",
-              }}
-              className={`border ${isDarkMode
-                ? 'bg-[#3B4D66]' : 'bg-white '} flex h-16 justify-between items-center rounded-lg p-3 mb-3 cursor-pointer`}
-              onClick={() => !isSubmitted && setPendingOption(option)}
-            >
-              <div className="flex items-center gap-3">
-                <div
-                  className={`w-10 h-10 p-2 rounded-md flex justify-center items-center flex-shrink-0 ${isSubmitted && pendingOption === option
-                      ? option === currentQuestion.answer
-                        ? "bg-green-400"
-                        : "bg-red-500"
-                      : pendingOption === option
-                        ? "bg-purple-500"
-                        : "bg-gray-200"}`}
-                >
-                  <span
-                    className={`text-sm font-medium ${isSubmitted && pendingOption === option
-                        ? "text-white"
-                        : pendingOption === option
-                          ? "text-white"
-                          : "text-[#626C7F]"}`}
-                  >
-                    {labels[index]}
-                  </span>
-                </div>
-                {option}
-              </div>
-
-              {answered && (
-                <span className="flex ">
-                  <img
-                    src={option === currentQuestion.answer
-                      ? correct
-                      : selectedOption === option
-                        ? incorrect
-                        : undefined}
-                    alt="" />
-                </span>
-              )}
-            </li>
+      <div className="flex flex-col lg:flex-row lg:gap-32">
+        <div className="flex flex-col lg:justify-between lg:h-[340px] lg:w-[465px]">
+          <div>
+            <p className="mb-3 text-[#626C7F]">
+              <em
+                className={`${
+                  isDarkMode ? "text-[#ABC1E1]" : "text-[#626C7F]"
+                } font-light`}
+              >
+                Question {currentQuestionIndex + 1} of {quiz.length}
+              </em>
+            </p>
+            <h3 className="text-2xl font-semibold mb-6">
+              {currentQuestion.question}
+            </h3>
           </div>
-        ))}
-        {!isSubmitted ? (
-          <button
-            className="bg-purple-500 py-4 text-white w-full rounded-lg"
-            onClick={handleSubmitAnswer}
-            disabled={!pendingOption}
+
+          <div
+            className={`w-full ${
+              isDarkMode ? "bg-[#3B4D66]" : "bg-white "
+            } rounded-full h-3 mb-10 flex items-center`}
           >
-            Submit Answer
-          </button>
-        ) : (
-          <button
-            className="bg-purple-500 py-4 text-white w-full px-4  rounded-lg"
-            onClick={handleNextQuestion}
-          >
-            Next Question
-          </button>
-        )}
-      </ul>
+            <div
+              className="bg-purple-500 h-2 rounded-full "
+              style={{ width: `${progressPercentage}%` }}
+            ></div>
+          </div>
+        </div>
+        <div className="flex flex-col lg:flex-grow">
+          <ul className="mb-4">
+            {currentQuestion.options.map((option, index) => (
+              <div key={index}>
+                <li
+                  style={{
+                    border:
+                      isSubmitted && pendingOption === option
+                        ? `2px solid ${
+                            option === currentQuestion.answer ? "green" : "red"
+                          }`
+                        : pendingOption === option
+                        ? "2px solid #A729F5"
+                        : "transparent",
+                  }}
+                  className={`border ${
+                    isDarkMode ? "bg-[#3B4D66]" : "bg-white "
+                  } flex h-16 justify-between items-center rounded-lg p-3 mb-3 cursor-pointer`}
+                  onClick={() => !isSubmitted && setPendingOption(option)}
+                >
+                  <div className="flex items-center gap-3">
+                    <div
+                      className={`w-10 h-10 p-2 rounded-md flex justify-center items-center flex-shrink-0 ${
+                        isSubmitted && pendingOption === option
+                          ? option === currentQuestion.answer
+                            ? "bg-green-400"
+                            : "bg-red-500"
+                          : pendingOption === option
+                          ? "bg-purple-500"
+                          : "bg-gray-200"
+                      }`}
+                    >
+                      <span
+                        className={`text-sm font-medium ${
+                          isSubmitted && pendingOption === option
+                            ? "text-white"
+                            : pendingOption === option
+                            ? "text-white"
+                            : "text-[#626C7F]"
+                        }`}
+                      >
+                        {labels[index]}
+                      </span>
+                    </div>
+                    {option}
+                  </div>
+                  {answered && (
+                    <span className="flex ">
+                      <img
+                        src={
+                          option === currentQuestion.answer
+                            ? correct
+                            : selectedOption === option
+                            ? incorrect
+                            : undefined
+                        }
+                        alt=""
+                      />
+                    </span>
+                  )}
+                </li>
+              </div>
+            ))}
+            {!isSubmitted ? (
+              <button
+                className="bg-purple-500 py-4 text-white w-full rounded-lg"
+                onClick={handleSubmitAnswer}
+                disabled={!pendingOption}
+              >
+                Submit Answer
+              </button>
+            ) : (
+              <button
+                className="bg-purple-500 py-4 text-white w-full px-4  rounded-lg"
+                onClick={handleNextQuestion}
+              >
+                Next Question
+              </button>
+            )}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
-
